d99195d9fdf243b74ed0e1e0f07aa64a6489d23f
Alex Hall <alex.mojaki@gmail.com>
2023-09-18 13:23:50 +0200

    Move initial python code to separate file

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 import * as Comlink from 'comlink';
+import initKernelPy from './initKernelPy';
 
 const pendingWorkers: MyWorker[] = [];
 
@@ -81,48 +82,7 @@ const plugin: JupyterFrontEndPlugin<void> = {
       console.log('JupyterLab extension grist-widget is activated!');
 
       const kernel = await getKernel(app);
-      kernel.requestExecute({
-        code: `
-from pyodide.ffi import to_js
-import js
-import pyodide_js
-
-
-class ComlinkProxy:
-    def __init__(self, proxy):
-        self.proxy = proxy
-
-    def __getattr__(self, name):
-        return ComlinkProxy(getattr(self.proxy, name))
-
-    async def __call__(self, *args, **kwargs):
-        args = [
-            to_js(arg, dict_converter=js.Object.fromEntries)
-            for arg in args
-        ]
-        kwargs = {
-            key: to_js(value, dict_converter=js.Object.fromEntries)
-            for key, value in kwargs.items()
-        }
-        result = await self.proxy(*args, **kwargs)
-        if hasattr(result, "to_py"):
-            result = result.to_py()
-        return result
-
-
-__grist_plugin__ = None
-
-def __get_grist_plugin():
-    global __grist_plugin__
-    if __grist_plugin__ is not None:
-        return __grist_plugin__
-
-    js.importScripts("https://unpkg.com/comlink@4.4.1/dist/umd/comlink.js")
-    pyodide_js.registerComlink(js.Comlink)
-    __grist_plugin__ = ComlinkProxy(js.Comlink.wrap(js).grist)
-    return __grist_plugin__
-`
-      });
+      kernel.requestExecute({ initKernelPy });
 
       for (const worker of pendingWorkers) {
         exposeWorker(worker, grist);
---
+++ src/initKernelPy.ts
@@ -0,0 +1,43 @@
+// language=Python
+const code = `
+from pyodide.ffi import to_js
+import js
+import pyodide_js
+
+
+class ComlinkProxy:
+  def __init__(self, proxy):
+      self.proxy = proxy
+
+  def __getattr__(self, name):
+      return ComlinkProxy(getattr(self.proxy, name))
+
+  async def __call__(self, *args, **kwargs):
+      args = [
+          to_js(arg, dict_converter=js.Object.fromEntries)
+          for arg in args
+      ]
+      kwargs = {
+          key: to_js(value, dict_converter=js.Object.fromEntries)
+          for key, value in kwargs.items()
+      }
+      result = await self.proxy(*args, **kwargs)
+      if hasattr(result, "to_py"):
+          result = result.to_py()
+      return result
+
+
+__grist_plugin__ = None
+
+def __get_grist_plugin():
+  global __grist_plugin__
+  if __grist_plugin__ is not None:
+      return __grist_plugin__
+
+  js.importScripts("https://unpkg.com/comlink@4.4.1/dist/umd/comlink.js")
+  pyodide_js.registerComlink(js.Comlink)
+  __grist_plugin__ = ComlinkProxy(js.Comlink.wrap(js).grist)
+  return __grist_plugin__
+`;
+
+export default code;
