cf7f4f662ac786a1f1ef24e44960f984e556583b
Alex Hall <alex.mojaki@gmail.com>
2023-09-20 11:21:13 +0200

    make onRecord(s) fetch records so that they stay encoded

--- src/initKernelPy.ts
+++ src/initKernelPy.ts
@@ -16,13 +16,24 @@ def __make_grist_api():
         async def __call__(self, *args, **kwargs):
             if any(callable(arg) for arg in args):
                 assert len(args) == 1 and not kwargs, "Only one callable argument is supported"
-
-                def wrapper(*callback_args):
+                [callback] = args
+                name = self._name
+                async def wrapper(*callback_args):
                     callback_args = [
                         a.to_py() if hasattr(a, "to_py") else a
                         for a in callback_args
                     ]
-                    return args[0](*callback_args)
+                    if name == 'onRecord':
+                        record, *rest = callback_args
+                        if record:
+                            record = await grist.fetchSelectedRecord(record['id'], keepEncoded=True)
+                        callback(record, *rest)
+                    elif name == 'onRecords':
+                        _, *rest = callback_args
+                        records = await grist.fetchSelectedTable(keepEncoded=True)
+                        callback(records, *rest)                    
+                    else:
+                        callback(*callback_args)
 
                 js._grist_tmp1 = self._proxy
                 js._grist_tmp2 = js.Comlink.proxy(create_proxy(wrapper))
